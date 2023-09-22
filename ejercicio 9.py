54bec3e7dbbbef07bb63822cd8a6b7c5296d30b1
Helenn12 <144930801+Helenn12@users.noreply.github.com>
2023-09-22 10:38:20 -0500

    Add files via upload
    ejercicio 9

---
+++ ejercicio 9.py
@@ -0,0 +1,40 @@
+#vamos a realizar un programa que realice una tupla compuesta por diferentes 
+#Tipos de datos que incluyan variables adicionalmente que esten orientados a realizar
+#diferentes acciones enfocadas en generar resultados en variaciones
+
+print ("Este es un programa de tuplas")
+
+#n = [(1,4),(4,8)]
+#t = [("Emanuel",1),(34,"hogar")]
+
+#print (n,t)
+
+#res = ()
+
+#for n in res:
+    #print (n)
+    #if n == res
+    #print (res)
+
+#localiozar dentro de una tupla la posicion determinada por un operador de referencia como el:
+B = ("Helen" , "hola", "trabajo" , [10, 11, 12, 13] , "Alejandra", "billar", "'pasaporte", "dinero")
+#B = (10,11,12,13,14,15,16,17,18)
+#res = ()
+
+#print (A[1:4])
+
+#or A in True: 
+    #print (A)
+    #if A == True:
+        #print ("casa")
+
+print (B[0:1])
+print (B[2:8])
+print (B[2:4])
+print (B[3:9])
+
+#for B in res:
+    #print (B)
+    #if B == True 
+     #print ("Helen")
+
