9dc584380a2203647b35f5b781168d061744c75e
Helenn12 <144930801+Helenn12@users.noreply.github.com>
2023-09-27 09:24:46 -0500

    Add files via upload

---
+++ proyecto.py
@@ -0,0 +1,80 @@
+fórmulas_quimicas = {
+    "Nombre": "Hidrógeno",
+    "Símbolo": "H",
+    "Número atómico": 1,
+    "Fórmula química": "H2"
+}
+
+fórmulas_A = {
+    "Nombre": "Lantano",
+    "Símbolo": "La",
+    "Número atómico": 57,
+    "Fórmula química": "La2O3"
+}
+
+fórmulas_B = {
+    "Nombre": "Tántalo",
+    "Símbolo": "Ta",
+    "Número atómico": 73,
+    "Fórmula química": "Ta2O5"
+}
+
+fórmulas_C = {
+    "Nombre": "Renio",
+    "Símbolo": "Re",
+    "Número atómico": 75,
+    "Fórmula química": "NH4Re04"
+}
+
+fórmulas_D = {
+    "Nombre": "Osmio",
+    "Símbolo": "Os",
+    "Número atómico": 76,
+    "Fórmula química": "OsO4"
+}
+
+fórmulas_E = {
+    "Nombre": "Iridio",
+    "Símbolo": "Ir",
+    "Número atómico": 77,
+    "Fórmula química": "IrO2"
+}
+
+fórmulas_F = {
+    "Nombre": "Platino",
+    "Símbolo": "Pt",
+    "Número atómico": 78,
+    "Fórmula química": "Pt"
+}
+
+fórmulas_G = {
+    "Nombre": "Paladio",
+    "Símbolo": "Pd",
+    "Número atómico": 46,
+    "Fórmula química": "Pd"
+}
+
+fórmulas_H = {
+    "Nombre": "Estaño",
+    "Símbolo": "Sn",
+    "Número atómico": 50,
+    "Fórmula química": "SnO2"
+}
+
+fórmulas_I = {
+    "Nombre": "Telurio",
+    "Símbolo": "Te",
+    "Número atómico": 52,
+    "Fórmula química": "Te"
+}
+
+print("Hidrógeno", fórmulas_quimicas)
+print("Lantano", fórmulas_A)
+print("Tántalo", fórmulas_B)
+print("Renio", fórmulas_C)
+print("Osmio", fórmulas_D)
+print("Iridio", fórmulas_E)
+print("Platino", fórmulas_F)
+print("Paladio", fórmulas_G)
+print("Estaño", fórmulas_H)
+print("Telurio", fórmulas_I)
