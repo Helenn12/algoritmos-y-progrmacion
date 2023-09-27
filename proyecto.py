b4ea35e24d2d2199b1cba288d354780a4a7b1c0b
Helenn12 <144930801+Helenn12@users.noreply.github.com>
2023-09-27 10:01:56 -0500

    Add files via upload

--- proyecto.py
+++ proyecto.py
@@ -2,85 +2,81 @@
 #Se deben crear un diccionario completamente diferente en cada elemento 
 #Cada diccionario debe estar cerrado y ser completamente unico
 
-fórmulas_quimicas = {
-    "Nombre": "Hidrógeno",
-    "Símbolo": "H",
-    "Número atómico": 1,
-    "Fórmula química": "H2"
+# Diccionario de fórmulas químicas
+
+formulas_quimicas = {
+    "Hidrógeno": {
+        "Nombre": "Hidrógeno",
+        "Símbolo": "H",
+        "Número atómico": 1,
+        "Fórmula química": "H2"
+    },
+    "Lantano": {
+        "Nombre": "Lantano",
+        "Símbolo": "La",
+        "Número atómico": 57,
+        "Fórmula química": "La2O3"
+    },
+    "Tántalo": {
+        "Nombre": "Tántalo",
+        "Símbolo": "Ta",
+        "Número atómico": 73,
+        "Fórmula química": "Ta2O5"
+    },
+    "Renio": {
+        "Nombre": "Renio",
+        "Símbolo": "Re",
+        "Número atómico": 75,
+        "Fórmula química": "NH4Re04"
+    },
+    "Osmio": {
+        "Nombre": "Osmio",
+        "Símbolo": "Os",
+        "Número atómico": 76,
+        "Fórmula química": "OsO4"
+    },
+    "Iridio": {
+        "Nombre": "Iridio",
+        "Símbolo": "Ir",
+        "Número atómico": 77,
+        "Fórmula química": "IrO2"
+    },
+    "Platino": {
+        "Nombre": "Platino",
+        "Símbolo": "Pt",
+        "Número atómico": 78,
+        "Fórmula química": "Pt"
+    },
+    "Paladio": {
+        "Nombre": "Paladio",
+        "Símbolo": "Pd",
+        "Número atómico": 46,
+        "Fórmula química": "Pd"
+    },
+    "Estaño": {
+        "Nombre": "Estaño",
+        "Símbolo": "Sn",
+        "Número atómico": 50,
+        "Fórmula química": "SnO2"
+    },
+    "Telurio": {
+        "Nombre": "Telurio",
+        "Símbolo": "Te",
+        "Número atómico": 52,
+        "Fórmula química": "Te"
+    }
 }
 
-fórmulas_A = {
-    "Nombre": "Lantano",
-    "Símbolo": "La",
-    "Número atómico": 57,
-    "Fórmula química": "La2O3"
-}
+# Bucle interactivo para buscar elementos
+while True:
+    elemento = input("Introduce el nombre del elemento (o 'salir' para finalizar): ")
+    if elemento.lower() == 'salir':
+        break
+    if elemento in formulas_quimicas:
+        informacion_elemento = formulas_quimicas[elemento]
+        print(f"Información del elemento {elemento}:")
+        for clave, valor in informacion_elemento.items():
+            print(f"{clave}: {valor}")
+    else:
+        print("Elemento no encontrado. Por favor, verifica el nombre del elemento.")
 
-fórmulas_B = {
-    "Nombre": "Tántalo",
-    "Símbolo": "Ta",
-    "Número atómico": 73,
-    "Fórmula química": "Ta2O5"
-}
-
-fórmulas_C = {
-    "Nombre": "Renio",
-    "Símbolo": "Re",
-    "Número atómico": 75,
-    "Fórmula química": "NH4Re04"
-}
-
-fórmulas_D = {
-    "Nombre": "Osmio",
-    "Símbolo": "Os",
-    "Número atómico": 76,
-    "Fórmula química": "OsO4"
-}
-
-fórmulas_E = {
-    "Nombre": "Iridio",
-    "Símbolo": "Ir",
-    "Número atómico": 77,
-    "Fórmula química": "IrO2"
-}
-
-fórmulas_F = {
-    "Nombre": "Platino",
-    "Símbolo": "Pt",
-    "Número atómico": 78,
-    "Fórmula química": "Pt"
-}
-
-fórmulas_G = {
-    "Nombre": "Paladio",
-    "Símbolo": "Pd",
-    "Número atómico": 46,
-    "Fórmula química": "Pd"
-}
-
-fórmulas_H = {
-    "Nombre": "Estaño",
-    "Símbolo": "Sn",
-    "Número atómico": 50,
-    "Fórmula química": "SnO2"
-}
-
-fórmulas_I = {
-    "Nombre": "Telurio",
-    "Símbolo": "Te",
-    "Número atómico": 52,
-    "Fórmula química": "Te"
-}
-
-#se debe imprimir cada lista para que el sistema lo reconozca 
-
-print("Hidrógeno", fórmulas_quimicas)
-print("Lantano", fórmulas_A)
-print("Tántalo", fórmulas_B)
-print("Renio", fórmulas_C)
-print("Osmio", fórmulas_D)
-print("Iridio", fórmulas_E)
-print("Platino", fórmulas_F)
-print("Paladio", fórmulas_G)
-print("Estaño", fórmulas_H)
-print("Telurio", fórmulas_I)
